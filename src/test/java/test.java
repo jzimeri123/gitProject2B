ac425dfbe468a67b98d34e13188be0931c8ff76c
jzimeri123 <jzimeri@gmail.com>
2025-07-22 09:20:16 -0400

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("this is the project version 1");
+    }
+}
+//done working version 1
